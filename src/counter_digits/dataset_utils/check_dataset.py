4a14436093f84582fbda6505ac77c0fbb96200fd
trevol <iamvovan@mail.ru>
2020-07-26 18:24:02 +0300

    checking dataset

--- src/counter_digits/dataset_utils/check_dataset.py
+++ src/counter_digits/dataset_utils/check_dataset.py
@@ -1,11 +1,39 @@
 import cv2
-import pathlib
+from pathlib import Path
 import os
+
+from trvo_utils import toInt_array, toInt
+from trvo_utils.annotation import PascalVocXmlParser
+from trvo_utils.imutils import imshowWait, imSize, fit_image_to_shape
+
+from counter_digits.dataset_utils.extract_dataset import list_files
+
+
+def display(imgFile, annotationFile):
+    img = cv2.imread(imgFile)
+    p = PascalVocXmlParser(annotationFile)
+    color = 255, 0, 0
+    img, scale = fit_image_to_shape(img, (950, 1850))
+    for x1, y1, x2, y2 in p.boxes():
+        if scale < 1:
+            x1, y1, x2, y2 = toInt(x1 * scale, y1 * scale, x2 * scale, y2 * scale)
+        else:
+            x1, y1, x2, y2 = toInt(x1, y1, x2, y2)
+
+        cv2.rectangle(img, (x1, y1), (x2, y2), color, 1)
+    return imshowWait([img, imgFile])
 
 
 def check(dirConfigs):
+    imagesExtensions = ['jpg', 'jpeg', 'png']
     for directory, img2annotationFn in dirConfigs:
-        pass
+        for imgFile in list_files([directory], imagesExtensions):
+            annotationFile = img2annotationFn(imgFile)
+            if not os.path.isfile(annotationFile):
+                continue
+            key = display(imgFile, annotationFile)
+            if key == 27:
+                return
 
 
 def splitPath(path):
@@ -18,27 +46,23 @@ def splitPath(path):
     return parentDir, baseName, nameWithoutExt, ext
 
 
-def inplaceAnnotation(imgPath):
-    parentDir, baseName, nameWithoutExt, ext = splitPath(imgPath)
-    return os.path.join(parentDir, nameWithoutExt + ".xml")
-
-
-def annotation_at_digits_annotations(imgPath):
-    parentDir, baseName, nameWithoutExt, ext = splitPath(imgPath)
-    return os.path.join(parentDir, "digits_annotations", nameWithoutExt + ".xml")
-
-
 if __name__ == '__main__':
-    dirConfigs = [
-        ("/hdd/Datasets/counters/0_from_internet/train", inplaceAnnotation),
-        ("/hdd/Datasets/counters/0_from_internet/val", inplaceAnnotation),
-        ("/hdd/Datasets/counters/1_from_phone/train", inplaceAnnotation),
-        ("/hdd/Datasets/counters/1_from_phone/val", inplaceAnnotation)
-    ]
-
-
     def check_main():
-        pass
+        def inplaceAnnotation(imgPath):
+            parentDir, baseName, nameWithoutExt, ext = splitPath(imgPath)
+            return os.path.join(parentDir, nameWithoutExt + ".xml")
+
+        def annotation_at_digits_annotations(imgPath):
+            parentDir, baseName, nameWithoutExt, ext = splitPath(imgPath)
+            return os.path.join(parentDir, "digits_annotations", nameWithoutExt + ".xml")
+
+        dirConfigs = [
+            # ("/hdd/Datasets/counters/0_from_internet/train", inplaceAnnotation),
+            # ("/hdd/Datasets/counters/0_from_internet/val", inplaceAnnotation),
+            ("/hdd/Datasets/counters/1_from_phone/train", inplaceAnnotation),
+            ("/hdd/Datasets/counters/1_from_phone/val", inplaceAnnotation)
+        ]
+        check(dirConfigs)
 
 
     check_main()
