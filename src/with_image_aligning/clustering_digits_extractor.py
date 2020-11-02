ccd6de469a734e165526cf1d331330cd747a7fe1
trevol <iamvovan@mail.ru>
2020-11-02 18:29:05 +0300

    start aggregate (cluster) digits detection by box centers

--- src/with_image_aligning/clustering_digits_extractor.py
+++ src/with_image_aligning/clustering_digits_extractor.py
@@ -1,6 +1,9 @@
 from dataclasses import dataclass
 from typing import List
+
+import hdbscan
 import numpy as np
+
 from detection.TwoStageDigitsDetectionResult import DigitDetection
 
 
@@ -11,5 +14,9 @@ class DigitAtPoint:
 
 
 class DigitsExtractorClustering:
-    def extract(self, detections: List[DigitDetection]):
+    def __init__(self):
+        self.clusterer = hdbscan.HDBSCAN()
+
+    def extract(self, detections: List[DigitDetection]) -> List[DigitAtPoint]:
+        # cluster
         pass
