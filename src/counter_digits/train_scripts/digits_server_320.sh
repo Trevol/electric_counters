1c703b731c8e88bbe0f49dc17d7823a7386d4d37
trevol <iamvovan@mail.ru>
2020-11-10 21:25:00 +0300

    experiments with augmentation

--- src/counter_digits/train_scripts/digits_server_320.sh
+++ src/counter_digits/train_scripts/digits_server_320.sh
@@ -1,5 +1,5 @@
 source train_common.sh
 
 python ../../train.py --cfg ../data/yolov3-tiny-10cls-320.cfg \
-       --data ../data/digits.data --weights ../../../weights/yolov3-tiny.pt --device 0 --epochs 5000 --batch-size 16 \
+       --data ../data/digits.data --weights ../../../weights/yolov3-tiny.pt --device 0 --epochs 10000 --batch-size 16 \
        --img-size 288 608 320 --input-size 320 --rect --multi-scale
