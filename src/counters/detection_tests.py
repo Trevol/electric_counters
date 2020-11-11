1d58fbfd13f6a3931cf88fc03b279ed7d25928c4
trevol <iamvovan@mail.ru>
2020-11-11 14:16:29 +0300

    experiments with augmentation

--- src/counters/detection_tests.py
+++ src/counters/detection_tests.py
@@ -5,6 +5,7 @@ from trvo_utils import toInt_array
 from trvo_utils.cv2gui_utils import imshowWait
 from trvo_utils.imutils import imreadRGB, rgb2bgr, bgr2rgb, fit_image_to_shape
 
+from detection.DarknetOpencvDetector import DarknetOpencvDetector
 from detection.DarknetPytorchDetector import DarknetPytorchDetector
 from consts import BGRColors, FHD_SHAPE
 from utils_local.vis_utils import drawDetections, fitImageDetectionsToShape
@@ -19,24 +20,36 @@ def test_detect():
         # '/home/trevol/hdd/Datasets/counters/data/0_from_internet/all/*.jp*',
         # '/hdd/Datasets/counters/data/for_yolo/images/0_from_internet/train/*.jp*',
         # '/hdd/Datasets/counters/data/for_yolo/images/0_from_internet/val/*.jp*',
-        '/hdd/Datasets/counters/data/Musson_counters/train/*.jpg',
-        '/hdd/Datasets/counters/data/Musson_counters/val/*.jpg'
+        # '/hdd/Datasets/counters/data/Musson_counters/*.jpg',
+        # "/home/trevol/Repos/experiments_with_lightweight_detectors/electric_counters/images/smooth_frames/1/*.jpg",
+        # "/home/trevol/Repos/experiments_with_lightweight_detectors/electric_counters/images/smooth_frames/2/*.jpg"
+        "/home/trevol/Repos/experiments_with_lightweight_detectors/electric_counters/images/smooth_frames/3/*.jpg",
+        "/home/trevol/Repos/experiments_with_lightweight_detectors/electric_counters/images/smooth_frames/4/*.jpg"
     ]
 
     s = 320
-    detector = DarknetPytorchDetector(
-        cfg_path=f'data/yolov3-tiny-2cls-{s}.cfg',
-        weights_path=f'best_weights/yolov3-tiny-2cls/{s}/yolov3-tiny-2cls-{s}.weights',
-        input_size=(s, s),
-        device='cpu',
-        conf_thres=.3,
+    cfg_path = f'data/yolov3-tiny-2cls-{s}.cfg'
+    weights_path = f'best_weights/yolov3-tiny-2cls/{s}/2/best_.weights'
+    # detector = DarknetPytorchDetector(
+    #     cfg_path=cfg_path,
+    #     weights_path=weights_path,
+    #     input_size=(s, s),
+    #     device='cpu',
+    #     conf_thres=.3,
+    #     iou_thres=.4
+    # )
+    detector = DarknetOpencvDetector(
+        cfg_path=cfg_path,
+        weights_path=weights_path,
+        input_size=s,
+        conf_thres=.5,
         iou_thres=.4
     )
 
     for im_files in image_files:
         for image_file in sorted(glob(im_files)):
             img = cv2.imread(image_file)
-            detections = detector.detect(bgr2rgb(img))[0]
+            detections = detector.detect(bgr2rgb(img))
 
             img, detections, _ = fitImageDetectionsToShape(img, detections, FHD_SHAPE)
             drawDetections(img, detections, BGRColors.green)
@@ -49,7 +62,7 @@ def test_detect():
 def test_convert_pt_to_weights():
     from ultralytics_yolo.models import convert
 
-    cfg_file = 'data/counters/yolov3-tiny-2cls-320.cfg'
-    weights_file = 'best_weights/yolov3-tiny-2cls/320/yolov3-tiny-2cls-320.pt'
+    cfg_file = 'data/yolov3-tiny-2cls-320.cfg'
+    weights_file = 'best_weights/yolov3-tiny-2cls/320/2/best__.pt'
 
     convert(cfg_file, weights_file)
